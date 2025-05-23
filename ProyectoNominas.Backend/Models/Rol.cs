3332de82d89fe62d2f9e940beb3640e874ec7f3f
GuillermoGome2z <gomezguille391@.gmailcom>
2025-05-22 22:21:14 -0600

    Se creo el Modelo para el Rol

---
+++ ProyectoNominas.Backend/Models/Rol.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoNominas.Backend.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public List<UsuarioRol> UsuarioRoles { get; set; } = new();
+    }
+}
--- ProyectoNominas.Backend/obj/Debug/net8.0/ProyectoNominas.Backend.AssemblyInfo.cs
+++ ProyectoNominas.Backend/obj/Debug/net8.0/ProyectoNominas.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoNominas.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907953ac859f92d80ca1a47524fb4139b23d808d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b06b281012648599b3aeaf628fc08daa662644")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoNominas.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoNominas.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
