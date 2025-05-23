907953ac859f92d80ca1a47524fb4139b23d808d
GuillermoGome2z <gomezguille391@.gmailcom>
2025-05-22 22:16:18 -0600

    Se creo el modelo para Usuario

---
+++ ProyectoNominas.Backend/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoNominas.Backend.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string ContrasenaHash { get; set; } = string.Empty;
+        public bool EstaActivo { get; set; } = true;
+        public List<UsuarioRol> UsuarioRoles { get; set; } = new();
+    }
+}
