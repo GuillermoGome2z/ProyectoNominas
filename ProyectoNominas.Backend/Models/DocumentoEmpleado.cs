821ad00be64c0d8b0278fa342347ae1e52fe27b0
GuillermoGome2z <gomezguille391@.gmailcom>
2025-05-23 00:56:24 -0600

    Se creo el modelo pra DocumentoEmpleado

---
+++ ProyectoNominas.Backend/Models/DocumentoEmpleado.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoNominas.Backend.Models
+{
+    public class DocumentoEmpleado
+    {
+        public int Id { get; set; }
+
+        public int EmpleadoId { get; set; }
+        public Empleado Empleado { get; set; }
+
+        public int TipoDocumentoId { get; set; }
+        public TipoDocumento TipoDocumento { get; set; }
+
+        public string RutaArchivo { get; set; }
+        public DateTime FechaSubida { get; set; }
+    }
+}
