da91c5e6bbd2bfdff33e00c0087e8d79851a6b80
GuillermoGome2z <gomezguille391@.gmailcom>
2025-05-23 00:48:56 -0600

    Se creo el modelo para Empleado

---
+++ ProyectoNominas.Backend/Models/Empleado.cs
@@ -0,0 +1,23 @@
+﻿namespace ProyectoNominas.Backend.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string DPI { get; set; }
+        public string NIT { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaContratacion { get; set; }
+
+        public int DepartamentoId { get; set; }
+        public Departamento Departamento { get; set; }
+
+        public int PuestoId { get; set; }
+        public Puesto Puesto { get; set; }
+
+        public ICollection<DocumentoEmpleado> Documentos { get; set; }
+        public ICollection<DetalleNomina> DetallesNomina { get; set; }
+    }
+}
+
--- ProyectoNominas.Backend/obj/Debug/net8.0/ProyectoNominas.Backend.AssemblyInfo.cs
+++ ProyectoNominas.Backend/obj/Debug/net8.0/ProyectoNominas.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoNominas.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f37da53cab728177d35895542534ee35b3ac3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b790e72691d91928a9a64eb383a92e109962c90")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoNominas.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoNominas.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
