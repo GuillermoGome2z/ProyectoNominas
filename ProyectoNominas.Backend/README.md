1c03580f7690f47c1f57648ec10d157205c1e375
GuillermoGome2z <gomezguille391@.gmailcom>
2025-05-24 05:37:49 -0600

    Cambios locales antes del rebase

---
+++ ProyectoNominas.Backend/README.md
@@ -0,0 +1,13 @@
+# ProyectoNominas
+
+Sistema de nóminas desarrollado con ASP.NET Core, Entity Framework y Blazor.
+
+## Descripción
+Este sistema permite gestionar empleados, roles, departamentos y el cálculo de nóminas, con autenticación JWT.
+
+## Tecnologías
+- ASP.NET Core 8
+- Entity Framework Core
+- SQL Server
+- Blazor WebAssembly
+- GitHub Actions / Azure DevOps (CI/CD)
--- ProyectoNominas.Frontend/obj/Debug/net8.0/ProyectoNominas.Frontend.AssemblyInfo.cs
+++ ProyectoNominas.Frontend/obj/Debug/net8.0/ProyectoNominas.Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoNominas.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2aa785a488d1468be07255b8784fb61feba5e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoNominas.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoNominas.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
